67aaddc2f9ea62a244d64d3efc5431f5abb43667
Artur Sadov <Art.S.V.86@yandex.ru>
2021-05-18 22:32:42 +0400

    Change files

--- src/main/java/Manager/PostManager.java
+++ src/main/java/Manager/PostManager.java
@@ -6,19 +6,11 @@ public class PostManager {
     private Post[] posts;
 
 
-    public Post[] search() {
-        int ownerId;
-        String domain;
-        String query;
-        boolean ownersOnly;
-        int count = 20;
-        int offset;
+    public Post[] search(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset) {
         return null;
     }
 
-    public void delete() {
-        int ownerId;
-        int postId;
+    public void delete(int ownerId, int postId) {
     }
 
 }
